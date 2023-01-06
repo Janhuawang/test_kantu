0577046fe69509168bd97928ff7aac744fa1f226
jianhua <wangjianhua@tslsmart.com>
2023-01-06 11:39:11 +0800

    搭建登录页

--- KanTu/Controller/LoginController.swift
+++ KanTu/Controller/LoginController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
+    /// 账号
+    @IBOutlet weak var ftAccount: UITextField!
+    /// 密码
+    @IBOutlet weak var tfPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    /// 登录事件
+    /// - Parameter sender: <#sender description#>
+    @IBAction func onClickToLogin(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }
